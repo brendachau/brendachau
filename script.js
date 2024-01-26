dee9db1478929614e9e716289b407cd9e786d18c
brendachau <132229574+brendachau@users.noreply.github.com>
2024-01-25 18:18:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -105,4 +105,19 @@ function reveal() {
 }
 
 
-window.alert("Please view this page in desktop full screen. Thank you!");
+// modal alert
+window.addEventListener("load", function() {
+  this.setTimeout(
+    function open(event) {
+      document.querySelector(".popup").style.display = "block";
+    }, 500
+  )
+});
+
+document.querySelector("#close").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "none";
+})
+
+document.querySelector("#popup-a").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "none";
+})
